e8584e29454678383cca206ba41da30d78c16a2b
largecats <linfanxiaolinda@outlook.com>
2020-12-28 23:52:38 +0800

    updated

---
+++ data_structures_and_algorithms_in_python/chapter9/unsorted_priority_queue.py
@@ -0,0 +1,37 @@
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """Priority queue implemented with unsorted list."""
+    def _find_min(self):
+        """Utility function to find item with minimum key."""
+        if self.is_empty():
+            raise Empty("Priority queue is empty")
+        small = self._data.first()
+        walk = self._data.after(small)
+        while walk is not None:
+            if walk.element() < small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small
+
+    def __init__(self):
+        """Create a new empty Priority Queue."""
+        self._data = PositionalList()
+
+    def __len__(self):
+        """Return the number of items in the priority queue."""
+        return len(self._data)
+
+    def add(self, key, value):
+        """Add a key-value pair."""
+        self._data.add_last(self._Item(key, value))  # add_last() method of PositionalList
+
+    def min(self):
+        """Return but do not remove (k, v) tuple with minimum key."""
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+
+    def remove_min(self):
+        """Remove and return (k, v) tuple with minimum key."""
+        p = self._find_min()
+        item = self._data.delete(p)
+        return (item._key, item._value)
